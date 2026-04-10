f45707bac34a677744cca1fce592312f062f3e25
Henry Noto <hen228@lehigh.edu>
2026-04-10 18:26:19 -0400

    Fix Auth0 tenant domain typo in frontend config

--- frontend/uar-ui/src/main.tsx
+++ frontend/uar-ui/src/main.tsx
@@ -4,7 +4,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import App from './App.tsx'
 
-const defaultDomain = 'dev-pzmiocjfeo5mjnq2.us.auth0.com'
+const defaultDomain = 'dev-pzmiocjfeo5mjqn2.us.auth0.com'
 const defaultClientId = '1BZScmvum58uuFuMiegEMvsNMyj94754'
 const defaultAudience = 'https://uar-copilot-api'
 
